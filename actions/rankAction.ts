e7c23d34fb0b03c668ac30f31acabddc3561f370
aleph <angelsflyinhell@namespace.media>
2025-08-02 02:42:29 +0200

    feat: move drizzle to fetch function

--- actions/rankAction.ts
+++ actions/rankAction.ts
@@ -1,13 +1,13 @@
-import { db } from "@/db/drizzle";
+import { getDb } from "@/db/drizzle";
 import { ranking } from "@/db/schema";
 import { Ranking } from "@/types/rankingType";
 import { eq } from "drizzle-orm";
 
 export const getRanking = async (id: number) => {
-  const data = await db.select().from(ranking).where(eq(ranking.id, id));
+  const data = await getDb().select().from(ranking).where(eq(ranking.id, id));
   return data;
 };
 
 export const createRanking = async (rankingData: Ranking) => {
-  await db.insert(ranking).values(rankingData);
+  await getDb().insert(ranking).values(rankingData);
 };
--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined in environment variables.");
-}
+export function getDb() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in environment variables.");
+  }
 
-export const db = drizzle(process.env.DATABASE_URL);
+  return drizzle(process.env.DATABASE_URL);
+}
