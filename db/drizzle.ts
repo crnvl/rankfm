0f18df7057c077b35a29cbe0be27a1e343b4dbc0
aleph <angelsflyinhell@namespace.media>
2025-08-02 02:33:25 +0200

    feat: remove dotenv

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,4 @@
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
-
-config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined in environment variables.");
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,6 @@
-import { config } from 'dotenv';
 import { defineConfig } from "drizzle-kit";
+import { config } from "process";
 
-config({ path: '.env' });
 
 export default defineConfig({
   schema: "./db/schema.ts",
